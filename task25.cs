c594b5840eb3500e60d002c05e8024d1420ce3bd
Eva <eva.leona@mail.ru>
2022-11-21 11:00:28 +0300

    .\task25.cs

---
+++ task25.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int i = 0;
+        while (num > 10)
+        {
+            i = num % 10 + i;
+            num %= 10;
+        }
+        Console.WriteLine(num);
+    }
+}
